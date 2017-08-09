84a351ceafc67fb0321a93890ad30c34a2b9ea67
Cameron Devine <camerondtrain@gmail.com>
2017-08-09 13:25:42 -0700

    updated the example to work with either Python 2 or 3.

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
+from __future__ import print_function
 import NetFT
 sensor = NetFT.NetFT('192.168.0.10')
-print sensor.getMeasurement()
+print(sensor.getMeasurement())
 sensor.tare()
-print sensor.getMeasurement()
+print(sensor.getMeasurement())
