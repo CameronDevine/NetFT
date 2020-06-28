f60f6d6770a580b384d1c4c79ac088f88c03f028
Cameron Devine <camerondtrain@gmail.com>
2020-06-28 00:10:01 -0700

    bumped the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name = "NetFT",
-    version = "0.6.1",
+    version = "1.0.0",
     packages = find_packages(),
 	author = "Cameron Devine",
 	author_email = "camdev@uw.edu",
