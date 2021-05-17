5aae78d0066335cd11b88896999894b1393f3b23
Cameron Devine <camerondtrain@gmail.com>
2021-05-17 16:49:43 -0700

    bumping to v2.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name = "NetFT",
-    version = "2.0.0",
+    version = "2.0.1",
     packages = find_packages(),
 	author = "Cameron Devine",
 	author_email = "camdev@uw.edu",
